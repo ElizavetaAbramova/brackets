8f94bb91b3fd783cd3fc3c76786727922935f430
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2024-10-15 14:24:02 +0400

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = [];
+  const CLOSE_BRACKETS = new Map();
+  const SIMILAR_BRACKETS = [];
+
+  const stack = [];
+
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  for (let arr of bracketsConfig) {
+    if (arr[0] === arr[1]) {
+      SIMILAR_BRACKETS.push(arr[0]);
+    } else {
+      OPEN_BRACKETS.push(arr[0]);
+      CLOSE_BRACKETS.set(arr[1], arr[0]);
+    }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+      let currentBracket = str[i];
+      if (OPEN_BRACKETS.includes(currentBracket)) {
+        stack.push(currentBracket);
+      } else {
+
+        let topElem = stack[stack.length - 1];
+
+          if (SIMILAR_BRACKETS.includes(currentBracket) && topElem === currentBracket) {
+            stack.pop();
+          } else if (SIMILAR_BRACKETS.includes(currentBracket) && topElem !== currentBracket) {
+            stack.push(currentBracket);
+          } else if (CLOSE_BRACKETS.get(currentBracket) === topElem) {
+
+            stack.pop();
+
+          } else if (stack.length === 0) {
+            return false;
+          }
+      }
+    }
+
+  return stack.length === 0;
 }
